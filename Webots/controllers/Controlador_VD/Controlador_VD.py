a8805a32c1688ca2a9754f94fa9671517e093cc6
god19260 <68131294+god19260@users.noreply.github.com>
2024-02-12 19:03:17 -0600

    3 agentes, mapa 1

---
+++ Webots/controllers/Controlador_VD/Controlador_VD.py
@@ -0,0 +1,285 @@
+
+from controller import Robot, Emitter, Receiver
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+
+# Funciones Desarrolladas en este trabajo
+import Rot_Control
+import Explorar
+import Condiciones_Giro
+import Odometria
+import Graficas
+import Obstaculos
+import Trayectoria
+
+class Slave(Robot):
+    # get the time step of the current world.
+    timestep = 3
+    
+    # Especificaciones del mundo
+    size_x = 5.0 #m
+    size_y = 5.0 #m
+
+    #------------------ Características del robot ------------------
+    wheelRadius = 0.035 #radio en m 
+    distanceCenter = 0.0748664*2 #0.075 # distancia en m 
+    angulo = 90  # ángulo respecto marco de referencia entorno
+    
+    #--------------------------------------------------------------- 
+    
+    
+    #-------------------------- Odometría -------------------------- 
+
+    PS_Right_Anterior = 0  # Pocisión de rueda derecha en n-1
+    PS_Left_Anterior  = 0  # Pocisión de rueda izquierda en n-1
+
+    phi = angulo # rotz estimado respecto marco de referencia entorno
+
+    # posición centroide vehículo
+    xc = [0]
+    yc = [0]
+
+    # Distancia en tramos de linea recta
+    Distancias_lineaRecta = []
+    Angulos = []
+    Distancia_Total = 0
+    T_Exploracion_x = [0]
+    T_Exploracion_y = [0]
+    T_Exploracion_GPS_x = []
+    T_Exploracion_GPS_y = []
+    error_GPS_Estimado = []
+    #---------------------------------------------------------------  
+
+    #------------------------- Obstaculos --------------------------
+    rango_max_dsen = 199 #cm
+    Pared_x_ds0 = []
+    Pared_x_ds1 = []
+    Pared_x_ds2 = []
+    Pared_x_ds3 = []
+    Pared_x_ds4 = []
+    Pared_x_ds5 = []
+
+    Pared_y_ds0 = []
+    Pared_y_ds1 = []
+    Pared_y_ds2 = []
+    Pared_y_ds3 = []
+    Pared_y_ds4 = []
+    Pared_y_ds5 = []
+
+    # Espacio de trabajo
+    factorWS = 10 # cada recuadro del mapa es de dimensiones (factor x factor)
+    WS_x = []
+    WS_y = []
+    min_val_x = 0
+    min_val_y = 0
+    
+    #---------------------------------------------------------------
+
+    #------------------------- Trayectoria -------------------------
+    ruta_optima = []
+    objetivo_x = 0
+    objetivo_y = 0
+
+    puntos_mapa = []
+    puntos_mapa_x = []
+    puntos_mapa_y = []
+    cant_puntos_mapa = 0
+    #---------------------------------------------------------------
+
+
+    #--------------------- Definiciones previas --------------------
+    distanceSensors = []
+    #---------------------------------------------------------------  
+
+
+    def __init__(self):
+        super(Slave, self).__init__()
+        # Habilitar Motores
+        self.left_motor = self.getDevice('motor_1')
+        self.right_motor = self.getDevice('motor_2')
+        
+        self.left_motor.setPosition(float('inf'))
+        self.left_motor.setVelocity(0.0)
+        
+
+        self.right_motor.setPosition(float('inf'))
+        self.right_motor.setVelocity(0.0)
+
+        # Habilitar sensores de  position de motores
+        self.PositionSensor_left = self.getDevice('PositionSensor_1')
+        self.PositionSensor_left.enable(self.timestep)
+        self.PositionSensor_right = self.getDevice('PositionSensor_2')
+        self.PositionSensor_right.enable(self.timestep)
+
+        # Habilitar sensores de distancia
+        for dsnumber in range(0, 6):
+            self.distanceSensors.append(self.getDevice('ds_' + str(dsnumber)))
+            self.distanceSensors[-1].enable(self.timestep)
+        
+        # Habilitar compass
+        self.compass = self.getDevice('compass')
+        self.compass.enable(self.timestep)
+
+        # Habilitar GPS
+        self.GPS = self.getDevice('gps')
+        self.GPS.enable(self.timestep)
+
+    
+    def DatosSensores_Init(self):
+        self.PS_Right_StartValue =  self.PositionSensor_right.getValue()  # PS_Right_value --> position sensor right value
+        self.PS_Left_StartValue  =  self.PositionSensor_left.getValue()  # PS_Right_value --> position sensor right value
+
+        # Offset para valores GPS 
+        # Actualizar valores de GPS
+        self.GPS_values = self.GPS.getValues()
+        self.T_Exploracion_GPS_x.append(self.GPS_values[0]+self.size_x/2)
+        self.T_Exploracion_GPS_y.append(self.GPS_values[1]+self.size_y/2)
+
+        self.delta_GPS_Estimado_x = abs(self.T_Exploracion_GPS_x[0])
+        self.delta_GPS_Estimado_y = abs(self.T_Exploracion_GPS_y[0])
+
+
+    def DatosSensores(self):
+        # Actualizar sensores de distancia
+        self.ds0_value = self.distanceSensors[0].getValue()+8
+        self.ds1_value = self.distanceSensors[1].getValue()+8
+        self.ds2_value = self.distanceSensors[2].getValue()+8
+        self.ds3_value = self.distanceSensors[3].getValue()+8
+        self.ds4_value = self.distanceSensors[4].getValue()+8
+        self.ds5_value = self.distanceSensors[5].getValue()+8
+
+        # Actualizar sensor de compass y angulo de rotz
+        self.compass_values = self.compass.getValues()
+        radian = np.arctan2(self.compass_values[0],self.compass_values[1])
+        self.rotz_g = radian*180/np.pi     # Rotación en el eje z del vehículo en grados
+        self.rotz = radian                 # Rotación en el eje z del vehículo en radianes
+
+        # Actualizar valores de sensores de posición de las ruedas
+        self.PS_Right_value = self.PositionSensor_right.getValue()- self.PS_Right_StartValue   # PS_Right_value --> position sensor right value
+        self.PS_Left_value  = self.PositionSensor_left.getValue()- self.PS_Left_StartValue    # PS_Left_value --> position sensor left value 
+        
+        # Actualizar valores de GPS
+        self.GPS_values = self.GPS.getValues()
+        
+
+Agente_1 = Slave()
+
+# Configuración de emisor
+emitter = Agente_1.getDevice("emitter")
+emitter.setChannel(1)
+
+# Configuración de receptor
+receiver = Agente_1.getDevice("receiver")
+receiver.enable(Agente_1.timestep)
+
+if Agente_1.step(Agente_1.timestep) != -1:
+    None
+
+
+Agente_1.DatosSensores_Init() 
+Agente_1.DatosSensores()
+
+# inicializar el ángulo de rotación del vehículo
+Agente_1.angulo = Agente_1.rotz_g
+Agente_1.phi = Agente_1.angulo
+
+Odometria.Odometria_Init(Agente_1)
+
+Explorar.Rutina_Inicio(Agente_1)
+
+tiempos = [2] # minutos 
+u = 0
+while Agente_1.step(Agente_1.timestep) != -1:
+    Agente_1.DatosSensores()
+    Rot_Control.Rot_Control(Agente_1.angulo,Agente_1)
+    Explorar.Explorar(Agente_1)
+    
+    
+
+    if Agente_1.getTime() >= tiempos[u]*60:
+        #------ Enviar datos a supervisor ----
+        data = "Robot: Hola"
+        
+        T_Exploracion_x_str = ' '.join(map(str, Agente_1.Pared_x_ds0))
+        T_Exploracion_y_str = ' '.join(map(str, Agente_1.Pared_y_ds0))  
+        
+        T_Exploracion_x_str =T_Exploracion_x_str+' '+ ' '.join(map(str, Agente_1.Pared_x_ds1))
+        T_Exploracion_y_str =T_Exploracion_y_str+' '+ ' '.join(map(str, Agente_1.Pared_y_ds1))  
+        
+        T_Exploracion_x_str =T_Exploracion_x_str+' '+ ' '.join(map(str, Agente_1.Pared_x_ds2))
+        T_Exploracion_y_str =T_Exploracion_y_str+' '+ ' '.join(map(str, Agente_1.Pared_y_ds2))
+
+        T_Exploracion_x_str =T_Exploracion_x_str+' '+ ' '.join(map(str, Agente_1.Pared_x_ds3))
+        T_Exploracion_y_str =T_Exploracion_y_str+' '+ ' '.join(map(str, Agente_1.Pared_y_ds3))
+
+        T_Exploracion_x_str =T_Exploracion_x_str+' '+ ' '.join(map(str, Agente_1.Pared_x_ds4))
+        T_Exploracion_y_str =T_Exploracion_y_str+' '+ ' '.join(map(str, Agente_1.Pared_y_ds4))
+
+        T_Exploracion_x_str =T_Exploracion_x_str+' '+ ' '.join(map(str, Agente_1.Pared_x_ds5))
+        T_Exploracion_y_str =T_Exploracion_y_str+' '+ ' '.join(map(str, Agente_1.Pared_y_ds5))
+
+        desfase_x =  str(Agente_1.T_Exploracion_GPS_x[0])
+        desfase_y =  str(Agente_1.T_Exploracion_GPS_y[0])
+
+        data =   desfase_x+ ' ' +desfase_y+ ' ' + T_Exploracion_x_str + ' '+ T_Exploracion_y_str
+        
+        emitter.send(data.encode())
+        
+        datos_obtenidos = False
+        while datos_obtenidos == False:
+            # ----- Recibir datos de supervisor -----
+            while receiver.getQueueLength() > 0:
+                data = receiver.getString().split()  # lectura de datos enviado por el supervisor del mundo webots
+                
+                primer_numero = float(data.pop(0))
+                segundo_numero = float(data.pop(0))
+                #print(data)
+
+                Agente_1.puntos_mapa = [float(x) for x in data]
+
+                Agente_1.cant_puntos_mapa = len(Agente_1.puntos_mapa) // 2
+                
+                Agente_1.puntos_mapa_x.extend([(x-float(desfase_x)) for x in Agente_1.puntos_mapa[:Agente_1.cant_puntos_mapa]])
+                Agente_1.puntos_mapa_y.extend([(y-float(desfase_y)) for y in Agente_1.puntos_mapa[Agente_1.cant_puntos_mapa:]])
+                
+                receiver.nextPacket()                
+               
+                if len(Agente_1.puntos_mapa_x) > 100000:
+                    datos_obtenidos = True
+                
+            
+            if Agente_1.step(Agente_1.timestep) == -1:
+                break
+        
+      
+        # Se considera este espacio el fin de la exploración
+        # y se genera la trayectoria al punto de inicio
+        Obstaculos.Espacio_Trabajo(Agente_1,1) # 0 para individual, 1 mapa completo
+
+
+        Agente_1.objetivo_x = (primer_numero+Agente_1.size_x/2.0)*Agente_1.factorWS-Agente_1.T_Exploracion_GPS_x[0]
+        Agente_1.objetivo_y = (segundo_numero+Agente_1.size_y/2.0)*Agente_1.factorWS-Agente_1.T_Exploracion_GPS_y[0]
+        Trayectoria.Ruta_Optima(Agente_1, Agente_1.objetivo_x, Agente_1.objetivo_y)
+
+        Graficas.graph_select(Agente_1)
+
+        if u == 0:
+            Explorar.Seguimiento_Trayectoria(Agente_1)
+
+        u += 1
+        try:
+            tiempos[u] = tiempos[u]+Agente_1.getTime()/60
+        except:
+            print("Tiempo de Simulación alcanzado.")
+            break
+
+    Condiciones_Giro.DecisionGiro(Agente_1)
+
+    if Agente_1.step(Agente_1.timestep) == -1:
+        break
+    
+
+
+
---
+++ Webots/controllers/Supervisor_Mundo/Supervisor_Mundo.py
@@ -0,0 +1,405 @@
+"""Supervisor_Mundo controller."""
+
+# You may need to import some classes of the controller module. Ex:
+#  from controller import Robot, Motor, DistanceSensor
+from controller import Supervisor, Receiver, Emitter
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+# create the Robot instance.
+robot = Supervisor()
+
+# get the time step of the current world.
+timestep = 3
+
+# Configuración de receptor
+receiver = robot.getDevice("receiver")
+receiver.enable(timestep)
+
+# Configuración de emisor
+emitter = robot.getDevice("emitter")
+emitter.setChannel(0)
+
+
+
+Walls = []
+translation_field = []
+orientation_field = []
+
+Obs = []
+Obs_Translation_Field = []
+Obs_Orientation_Field = []
+
+Checkpoints = []
+Checkpoints_Translation_Field = []
+Checkpoints_Orientation_Field = []
+Checkpoints_appearance = []
+
+
+cant_paredes = 10
+cant_cajas = 3
+cant_checkpoints = 1
+
+modo = 1 # 0 Obtener datos, 1 colocar paredes, 2 pruebas
+mapa = 1 # 0 no cambia mapa, 1 pruebas de algoritmos, 2 validación
+# -'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'- Obtener objetos del mundo -'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-
+# Paredes
+for dsnumber in range(cant_paredes):
+    Walls.append(robot.getFromDef('wall(' + str(dsnumber)+')'))
+    translation_field.append(Walls[-1].getField('translation'))
+    orientation_field.append(Walls[-1].getField('rotation'))
+
+# Obstaculos: Cajas
+for box_number in range(cant_cajas):
+    Obs.append(robot.getFromDef('BOX_'+str(box_number)))
+    Obs_Translation_Field.append(Obs[box_number].getField('translation'))
+    Obs_Orientation_Field.append(Obs[box_number].getField('rotation'))
+
+# Checkpoint
+for checkpoint_number in range(cant_checkpoints):
+    Checkpoints.append(robot.getFromDef('CP_'+str(checkpoint_number)))
+    Checkpoints_Translation_Field.append(Checkpoints[checkpoint_number].getField('translation'))
+    Checkpoints_Orientation_Field.append(Checkpoints[checkpoint_number].getField('rotation'))
+     
+    
+# -'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-
+
+
+# -'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'- Modos -'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-
+if modo == 0:
+    # Obtener datos de las paredes:
+    # Obtener las posiciones de las paredes 
+    print(" \nPosiciones de las paredes")
+    for i in range(len(Walls)):
+        print('wall(',i,'): ',Walls[i].getPosition())
+        
+    # Obtener la orientación de las paredes 
+    print(' \nOrientación de las paredes')
+    for i in range(len(Walls)):
+        print('wall(',i,'): ',orientation_field[i].getSFVec3f())
+
+    # Obtener datos de las cajas
+    # Obtener las posiciones de las cajas
+    print(" \nPosiciones de las cajas")
+    for i in range(len(Obs)):
+        print('Obstaculo Caja ',i,': ', Obs[i].getPosition())
+
+    # Obtener la orientación de las cajas
+    print(' \nOrientación de las cajas')
+    for i in range(len(Obs)):
+        print('Obstaculo Caja ',i,': ', Obs_Orientation_Field[i].getSFVec3f())
+
+elif modo == 1:
+    if mapa == 1:
+        # Mapa: Validación de algoritmos independientes
+        Walls_Positions =[[0.58, -1.09, 0.0],
+                [-2.13, -0.49999, 0.0],
+                [-1.09, 1.46, 0.0],
+                [-0.03, -2.32, 0.0],
+                [-0.130006, 0.49001, 0.0],
+                [0.33, -0.48, 0.0],
+                [0.300002, 1.50001, 0.0],
+                [0.76, 0.46, 0.0],
+                [-0.91, 2.46, 0.0],
+                [0.65999, 1.5, 0.0]]
+
+        Walls_Orientations = [[0.0, 0.0, 1.0, 0.0],
+                            [0.0, 0.0, 1.0, 1.5708],
+                            [0.0, 0.0, 1.0, 0.0],
+                            [0.0, 0.0, 1.0, 0.0],
+                            [0.0, 0.0, 1.0, 1.5708],
+                            [0.0, 0.0, 1.0, 0.0],
+                            [0.0, 0.0, 1.0, 1.5708],
+                            [0.0, 0.0, 1.0, 0.0],
+                            [0.0, 0.0, 1.0, 0.0],
+                            [0.0, 0.0, 1.0, 1.5708]]
+
+        for i in range(len(Walls)):
+            new_translation = Walls_Positions[i]
+            translation_field[i].setSFVec3f(new_translation)
+
+            new_orientation = Walls_Orientations[i]
+            orientation_field[i].setSFRotation(new_orientation)
+
+        # Obstaculos: Cajas
+        Obs_Positions =[[-0.632595, -1.325, 0.15],
+                        [1.38219, -0.0347126, 0.15],
+                        [1.76719, -1.75155, 0.15]]
+
+        Obs_Orientations = [[0.0, 0.0, 1.0, 0.523599],
+                            [0.0, 0.0, 1.0, 0.523599],
+                            [0.0, 0.0, -1.0, 4.692820414042842e-06]]
+
+        for i in range(len(Obs)):
+            new_translation = Obs_Positions[i]
+            Obs_Translation_Field[i].setSFVec3f(new_translation)
+
+            new_orientation = Obs_Orientations[i]
+            Obs_Orientation_Field[i].setSFRotation(new_orientation)
+    if mapa == 2:
+            # Mapa: Validación de algoritmos independientes
+        Walls_Positions =  [[0.57, 0.8, 0.0],
+                            [-1.16122, 1.144, 0.0],
+                            [0.09, -0.82, 0.0],
+                            [-0.98, -1.04, 0.0],
+                            [2.42999, 1.43001, 0.0],
+                            [0.57, -1.14, 0.0],
+                            [0.1, -0.16999, 0.0],
+                            [-1.42, -1.0, 0.0],
+                            [1.03001, -0.169991, 0.0],
+                            [1.86764, -1.83187, 0.0]]
+
+        Walls_Orientations =   [[0.0, 0.0, 1.0, -3.1415853071795863],
+                                [0.0, 0.0, -1.0, -0.7854053071795866],
+                                [0.0, 0.0, 1.0, -1.5707953071795862],
+                                [0.0, 0.0, 1.0, -1.5707953071795862],
+                                [0.0, 0.0, 1.0, 1.5708],
+                                [0.0, 0.0, 1.0, 1.01503e-06],
+                                [0.0, 0.0, 1.0, 1.5708],
+                                [0.0, 0.0, 1.0, 1.5708],
+                                [0.0, 0.0, 1.0, 1.5708],
+                                [0.0, 0.0, -1.0, -0.7854053071795866]]
+
+        for i in range(len(Walls)):
+            new_translation = Walls_Positions[i]
+            translation_field[i].setSFVec3f(new_translation)
+
+            new_orientation = Walls_Orientations[i]
+            orientation_field[i].setSFRotation(new_orientation)
+
+        # Obstaculos: Cajas
+        Obs_Positions =[[-0.812602, -0.965, 0.15],
+                        [-1.64782, -2.06471, 0.15],
+                        [1.50719, -0.56155, 0.15]]
+
+        Obs_Orientations = [[0.0, 0.0, 1.0, 1.5708],
+                            [0.0, 0.0, 1.0, 1.5708],
+                            [0.0, 0.0, -1.0, 4.692820414042842e-06]]
+
+        for i in range(len(Obs)):
+            new_translation = Obs_Positions[i]
+            Obs_Translation_Field[i].setSFVec3f(new_translation)
+
+            new_orientation = Obs_Orientations[i]
+            Obs_Orientation_Field[i].setSFRotation(new_orientation)
+    
+    if mapa == 3:
+            # Mapa: Validación de algoritmos independientes
+        Walls_Positions =  [[-1.64271, -2.10786, 0.0],
+                            [-2.03654, 0.618589, 0.0],
+                            [1.26, -0.72, 0.0],
+                            [-1.18,0.46,0],
+                            [2.40999, -1.51999, 0.0],
+                            [-0.4, -1.75, 0.0],
+                            [0.630468, 0.500919, 0.0],
+                            [1.01958, 1.49148, 0.0],
+                            [0.433445, -0.524993, 0.0],
+                            [2.04999, 1.6898, 0.0]]
+
+        Walls_Orientations =   [[0.0, 0.0, -1.0, -0.7854053071795866],
+                                [0.0, 0.0, -1.0, -1.3090053071795866],
+                                [0.0, 0.0, 1.0, -1.5707953071795862],
+                                [0.0, 0.0, -1.0, 1.0472],
+                                [0.0, 0.0, 1.0, 1.5708],
+                                [0.0, 0.0, 1.0, -1.5707953071795862],
+                                [0.0, 0.0, 1.0, 2.8798],
+                                [0.0, 0.0, 1.0, 2.618],
+                                [0.0, 0.0, 1.0, 2.0944],
+                                [0.0, 0.0, 1.0, 2.0944]]
+
+        for i in range(len(Walls)):
+            new_translation = Walls_Positions[i]
+            translation_field[i].setSFVec3f(new_translation)
+
+            new_orientation = Walls_Orientations[i]
+            orientation_field[i].setSFRotation(new_orientation)
+
+        # Obstaculos: Cajas
+        Obs_Positions =[[0.7374, -1.945, 0.15],
+                        [-0.227261, 1.6526, 0.15],
+                        [-1.24281, -0.731549, 0.15]]
+
+        Obs_Orientations = [[0.0, 0.0, 1.0, 1.5708],
+                            [0.0, 0.0, -1.0, -1.0472053071795866],
+                            [0.0, 0.0, 1.0, 1.30899]]
+
+        for i in range(len(Obs)):
+            new_translation = Obs_Positions[i]
+            Obs_Translation_Field[i].setSFVec3f(new_translation)
+
+            new_orientation = Obs_Orientations[i]
+            Obs_Orientation_Field[i].setSFRotation(new_orientation)
+    
+    if mapa == 4:
+            # Mapa: Validación de algoritmos independientes
+        Walls_Positions =  [[0.204959, -1.9413, 0.0],
+                            [1.13482, -1.11559, 0.0],
+                            [1.1902, 0.082823, 0.0],
+                            [1.08, -0.59, 0.0],
+                            [-0.22342, -0.96411, 0.0],
+                            [2.09, -1.64, 0.0],
+                            [-0.3, -0.02999, 0.0],
+                            [1.6, -2.08, 0.0],
+                            [2.42001, 0.840009, 0.0],
+                            [-2.44001, 1.43, 0.0]]
+
+        Walls_Orientations =   [[0.0, 0.0, 1.0, 2.618],
+                                [0.0, 0.0, 1.0, 2.3562],
+                                [0.0, 0.0, 1.0, -2.094395307179586],
+                                [0.0, 0.0, 1.0, -1.5707953071795862],
+                                [0.0, 0.0, -1.0, 2.87979],
+                                [0.0, 0.0, 1.0, 1.01503e-06],
+                                [0.0, 0.0, 1.0, 1.5708],
+                                [0.0, 0.0, 1.0, 1.5708],
+                                [0.0, 0.0, 1.0, 1.5708],
+                                [0.0, 0.0, 1.0, 1.5708]]
+
+        for i in range(len(Walls)):
+            new_translation = Walls_Positions[i]
+            translation_field[i].setSFVec3f(new_translation)
+
+            new_orientation = Walls_Orientations[i]
+            orientation_field[i].setSFRotation(new_orientation)
+
+        # Obstaculos: Cajas
+        Obs_Positions =[[-1.72259, -0.395, 0.15],
+                        [-1.50782, 1.60529, 0.15],
+                        [0.822977, 0.99345, 0.15]]
+
+        Obs_Orientations = [[0.0, 0.0, 1.0, 1.5708],
+                            [0.0, 0.0, 1.0, 1.5708],
+                            [0.0, 0.0, 1.0, -1.0472053071795866]]
+
+        for i in range(len(Obs)):
+            new_translation = Obs_Positions[i]
+            Obs_Translation_Field[i].setSFVec3f(new_translation)
+
+            new_orientation = Obs_Orientations[i]
+            Obs_Orientation_Field[i].setSFRotation(new_orientation)
+    
+    if mapa == 5:
+            # Mapa: Validación de algoritmos independientes
+        Walls_Positions =  [[-0.39377, -0.330622, 0.0],
+                            [2.45, 0.440027, 0.0],
+                            [-0.308399, -0.698404, 0.0],
+                            [0.182071, -0.896094, 0.0],
+                            [1.93999, -0.10999, 0.0],
+                            [1.59, 1.93, 0.0],
+                            [1.72351, 1.08352, 0.0],
+                            [-1.17785, -1.18105, 0.0],
+                            [-0.618382, -0.842897, 0.0],
+                            [1.03999, 1.43, 0.0]]
+
+        Walls_Orientations =   [[0.0, 0.0, 1.0, -2.3561953071795863],
+                                [0.0, 0.0, 1.0, 1.5708],
+                                [0.0, 0.0, -1.0, 0.785397],
+                                [0.0, 0.0, 1.0, -2.3561953071795863],
+                                [0.0, 0.0, 1.0, 1.5708],
+                                [0.0, 0.0, 1.0, -1.5707953071795862],
+                                [0.0, 0.0, 1.0, -2.3561953071795863],
+                                [0.0, 0.0, 1.0, 2.3562],
+                                [0.0, 0.0, 1.0, 2.3562],
+                                [0.0, 0.0, 1.0, 1.5708]]
+
+        for i in range(len(Walls)):
+            new_translation = Walls_Positions[i]
+            translation_field[i].setSFVec3f(new_translation)
+
+            new_orientation = Walls_Orientations[i]
+            orientation_field[i].setSFRotation(new_orientation)
+
+        # Obstaculos: Cajas
+        Obs_Positions =[[1.2774, -2.165, 0.15],
+                        [0.21218, 1.42529, 0.15],
+                        [-1.28281, 1.16845, 0.15]]
+
+        Obs_Orientations = [[0.0, 0.0, 1.0, 1.5708],
+                            [0.0, 0.0, 1.0, 1.8326],
+                            [0.0, 0.0, -1.0, 4.692820414042842e-06]]
+
+        for i in range(len(Obs)):
+            new_translation = Obs_Positions[i]
+            Obs_Translation_Field[i].setSFVec3f(new_translation)
+
+            new_orientation = Obs_Orientations[i]
+            Obs_Orientation_Field[i].setSFRotation(new_orientation)
+
+    # -'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-
+
+    # -'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-'-
+datos_recibidos = []
+puntos_mapa = []  # contiene los puntos recolectados por cada vehiculo, x e y juntos. lista tipo string
+
+puntos_mapa_x = [] # contiene las coordenadas x del mapa recolectado por todos los vehiculos. lista
+                   # tipo punto flotante
+puntos_mapa_y = [] # contiene las coordenadas y del mapa recolectado por todos los vehiculos. Lista
+                   # tipo punto flotante
+
+
+print(" ")
+print("-------------------------------------------------------")
+print("Al terninar el proceso de exploración y mapeo se podrán ")
+print("seleccionar las siguientes opciones. ")
+
+print("*Opciones con base a perspectiva individual del vehículo:")
+print("d: Mostrar datos")
+print("s: Mostrar porcentaje de error")
+print("1: Mostrar mapa")
+print("2: Mostrar obstaculos")
+print("*Opciones con base a perspectiva grupal de los vehículos:")
+print("3: Mostrar procesamiento de espacio de trabjo")
+print("4: Mostrar Espacio de trabajo")
+print("5: Mostrar ruta óptima de cada agente")
+print("n: Seguir la trayectoria (debe existir trayectoria).")   
+
+
+while robot.step(timestep) != -1:
+    
+    if receiver.getQueueLength() > 0:
+        
+        data = receiver.getString().split()  # lectura de datos enviado por los vehiculos
+        
+        
+        desfase_x = data.pop(0) 
+        desfase_y = data.pop(0)
+        
+        desfase_x = float(desfase_x)
+        desfase_y = float(desfase_y)
+           
+
+        puntos_mapa = [float(x) for x in data]
+
+        cant_puntos_mapa = len(puntos_mapa) // 2
+
+        puntos_mapa_x.extend([(desfase_x + x) for x in puntos_mapa[:cant_puntos_mapa]])
+        puntos_mapa_y.extend([(desfase_y + y) for y in puntos_mapa[cant_puntos_mapa:]])
+
+        
+        receiver.nextPacket()
+        #------ Enviar datos a robot ----
+        pos_checkpoint =Checkpoints[0].getPosition() 
+        x = ' '.join(map(str, puntos_mapa_x))
+        y = ' '.join(map(str, puntos_mapa_y))
+      
+        data = str(pos_checkpoint[0])+ ' ' +str(pos_checkpoint[1]) +' '+x+' '+y
+        emitter.send(data.encode())
+   
+        """
+        # graficar mapa de datos recibidos
+        
+        plt.plot((puntos_mapa_x),(puntos_mapa_y),'o',color = 'red')
+        plt.axhline(0, color='black',linewidth=0.5)
+        plt.axvline(0, color='black',linewidth=0.5)
+
+        
+        plt.xlabel('X')
+        plt.ylabel('Y')
+        plt.title('Mapa obtenido por todos los vehiculos')
+        plt.xlim(0,5)
+        plt.ylim(0,5)
+        plt.grid(True)
+        
+        plt.show()
+        """
